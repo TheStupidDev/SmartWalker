2654c5e3eb1aacaff3c032f8b624bc47c6c04163
TheStupidDev <98951500+TheStupidDev@users.noreply.github.com>
2022-03-20 15:24:43 +0700

    Update Sensor_SmartWalk.ino

--- Sensor_SmartWalk.ino
+++ Sensor_SmartWalk.ino
@@ -4,24 +4,23 @@ int const buzzer = 8;
 
 void setup()
 {
-  pinMode(bagian_pin, OUTPUT);
-  pinMode(bagian_echo, INPUT);
-  pinMode(buzzer, OUTPUT);
+	pinMode(bagian_pin, OUTPUT);
+	pinMode(bagian_echo, INPUT);
+	pinMode(buzzer, OUTPUT);
 }
 
 void loop()
 {
-  int durasi, jarak;
-  digitalWrite(bagian_pin, HIGH); 
-  delay(1);
-  digitalWrite(bagian_pin, LOW);
-  durasi = pulseIn(bagian_echo, HIGH);
-  jarak = (durasi/2) / 29.1; 
+	int durasi, jarak;
+	digitalWrite(bagian_pin, HIGH); 
+	delay(1);
+	digitalWrite(bagian_pin, LOW);
+	durasi = pulseIn(bagian_echo, HIGH);
+	jarak = (durasi/2) / 29.1; //dalam cm
     
-  if (jarak <= 20) {
-      tone(buzzer, 400);
+  if (jarak <= 30) {
+    	tone(buzzer, 400);
   } else {
-      noTone(buzzer);
+    	noTone(buzzer);
   }
-  delay(60);
 }
