2bd18351bedf14ea3b030676de30749342b9b40f
TheStupidDev <98951500+TheStupidDev@users.noreply.github.com>
2022-03-23 16:12:20 +0700

    Kode Program

--- Sensor_SmartWalk.ino
+++ Sensor_SmartWalk.ino
@@ -1,26 +1,31 @@
 int const bagian_pin = 7;
 int const bagian_echo = 6;
 int const buzzer = 8;
+int const vibrator = 13;
 
 void setup()
 {
-	pinMode(bagian_pin, OUTPUT);
-	pinMode(bagian_echo, INPUT);
-	pinMode(buzzer, OUTPUT);
+  pinMode(bagian_pin, OUTPUT);
+  pinMode(bagian_echo, INPUT);
+  pinMode(buzzer, OUTPUT);
+  pinMode(vibrator, OUTPUT);
 }
 
 void loop()
 {
-	int durasi, jarak;
-	digitalWrite(bagian_pin, HIGH); 
-	delay(1);
-	digitalWrite(bagian_pin, LOW);
-	durasi = pulseIn(bagian_echo, HIGH);
-	jarak = (durasi/2) / 29.1; //dalam cm
+  int durasi, jarak;
+  digitalWrite(bagian_pin, HIGH); 
+  delay(1);
+  digitalWrite(bagian_pin, LOW);
+  durasi = pulseIn(bagian_echo, HIGH);
+  jarak = (durasi/2) / 29.1; //dalam cm
     
-  if (jarak <= 30) {
-    	tone(buzzer, 2500);//Suara 2500 Hz
+  if (jarak <= 35) {
+      tone(buzzer, 2500);
+      analogWrite(vibrator, 255);
+    
   } else {
-    	noTone(buzzer);
+      noTone(buzzer);
+      analogWrite(vibrator, 0);
   }
 }
